22b299b923b23df2f0ec38d522c13c3c9dd7cdc8
Jayounghoyos <Juanandresyounghoyos@gmail.com>
2023-06-17 16:21:22 -0500

    Portafolio primer commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var animData = {
+    wrapper: document.querySelector('#animationWindow'),
+    animType: 'svg',
+    loop: true,
+    prerender: true,
+    autoplay: true,
+    path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
+  };
+
+var anim = bodymovin.loadAnimation(animData);
+anim.setSpeed(3.4);
+
+// Ocultar el elemento después de un tiempo determinado
+setTimeout(function() {
+    var animationWindow = document.querySelector('#animationWindow');
+    animationWindow.style.display = 'none';
+}, 1000); // 3000 ms = 3 segundos
+  
+window.addEventListener('load', function() {
+    setTimeout(function() {
+      var loadingOverlay = document.querySelector('#loadingOverlay');
+      loadingOverlay.style.display = 'none';
+    }, 1000); // 3000 ms = 3 segundos
+});
+  
