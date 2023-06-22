35f489a7d7cb324ea8b1b0e1b61d27f679a0e00e
Jayounghoyos <Juanandresyounghoyos@gmail.com>
2023-06-21 20:25:19 -0500

    Portafolio commit de subida

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var animData = {
-    wrapper: document.querySelector('#animationWindow'),
-    animType: 'svg',
-    loop: true,
-    prerender: true,
-    autoplay: true,
-    path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
-  };
+  wrapper: document.querySelector('#animationWindow'),
+  animType: 'svg',
+  loop: true,
+  prerender: true,
+  autoplay: true,
+  path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
+};
 
 var anim = bodymovin.loadAnimation(animData);
 anim.setSpeed(3.4);
@@ -14,12 +14,12 @@ anim.setSpeed(3.4);
 setTimeout(function() {
     var animationWindow = document.querySelector('#animationWindow');
     animationWindow.style.display = 'none';
-}, 1000); // 3000 ms = 3 segundos
+}, 3000); // 3000 ms = 3 segundos
   
 window.addEventListener('load', function() {
     setTimeout(function() {
       var loadingOverlay = document.querySelector('#loadingOverlay');
       loadingOverlay.style.display = 'none';
-    }, 1000); // 3000 ms = 3 segundos
+    }, 100); // 3000 ms = 3 segundos
 });
   
