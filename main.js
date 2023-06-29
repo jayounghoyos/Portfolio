69237ec5b899bd424b2c62f8134414997661b90f
Jayounghoyos <Juanandresyounghoyos@gmail.com>
2023-06-29 11:54:17 -0500

    cambio en las mediaquerys de los proyectos

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 var animData = {
-  wrapper: document.querySelector('#animationWindow'),
-  animType: 'svg',
+  wrapper: document.querySelector("#animationWindow"),
+  animType: "svg",
   loop: true,
   prerender: true,
   autoplay: true,
-  path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
+  path: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json",
 };
 
 var anim = bodymovin.loadAnimation(animData);
 anim.setSpeed(3.4);
 
 // Ocultar el elemento después de un tiempo determinado
-setTimeout(function() {
-    var animationWindow = document.querySelector('#animationWindow');
-    animationWindow.style.display = 'none';
+setTimeout(function () {
+  var animationWindow = document.querySelector("#animationWindow");
+  animationWindow.style.display = "none";
 }, 3000); // 3000 ms = 3 segundos
-  
-window.addEventListener('load', function() {
-    setTimeout(function() {
-      var loadingOverlay = document.querySelector('#loadingOverlay');
-      loadingOverlay.style.display = 'none';
-    }, 100); // 3000 ms = 3 segundos
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    var loadingOverlay = document.querySelector("#loadingOverlay");
+    loadingOverlay.style.display = "none";
+  }, 100); // 3000 ms = 3 segundos
 });
-  
